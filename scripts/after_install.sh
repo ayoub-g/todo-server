15aae7f8279ec59fbde5814e0b0e6965ba775d4c
Ayoub Gharbi <gharbi.ayoub@gmail.om>
2021-10-18 23:39:48 +0100

    fixed errors in appspec

--- scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-service codedeploy-agent start
-export AWS_REGION=eu-central-1
-export SECRET_ID=todoapp-rds
--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/
-chmod +x ./install
-./install auto > /tmp/logfile
-
