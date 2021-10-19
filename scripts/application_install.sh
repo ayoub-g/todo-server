ee49f3a7883fdea5cbef1046ceb002f08a36bf05
Ayoub Gharbi <gharbi.ayoub@gmail.om>
2021-10-19 17:34:16 +0100

    update scripts

--- scripts/application_install.sh
+++ scripts/application_install.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 cd /home/ubuntu/todo-app-server
 npm install
+exit 0
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/todo-app-server
 node index.js >app.out.log 2>app.err.log </dev/null &
+exit 0
