ff5a4471042e7875dd90b5187d6faeea0fc368c9
Ayoub Gharbi <gharbi.ayoub@gmail.om>
2021-10-20 07:30:23 +0100

    removed nodemon updated start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/todo-app-server
-npm start >app.out.log 2>app.err.log </dev/null &
+npm start > app.out.log 2>app.err.log </dev/null &
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu
 pkill node
-exit 0
